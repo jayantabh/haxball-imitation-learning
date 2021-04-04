f92410606c6ceb73a06a0e4db99cac1d9eaf28ae
Jonathan Leo <jpleo122@gmail.com>
2021-04-04 17:20:19 -0400

    Created 1v1 bot file that uses a given model to output it's actions.

--- run_bot.py
+++ run_bot.py
@@ -2,12 +2,13 @@ import argparse
 import haxball.async_common as async_common
 
 from bots.bot_example import ChasingBot
+from bots.my_model_bot import MyModelBot
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--channel", type=int, default=0, help="Different agents must be placed on different communication channels.")
 	args = parser.parse_args()
 	try:
-		async_common.run(ChasingBot(str(args.channel)).play())
+		async_common.run(MyModelBot(str(args.channel)).play())
 	except KeyboardInterrupt:
 		pass
